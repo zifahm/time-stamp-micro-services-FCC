46727ee68384a67e9db35dcce1a9f96c70377c00
hafiz <muhamm3dhaifz@gmail.com>
2018-01-25 19:05:39 +0530

    useless removed

---
+++ server.js
@@ -0,0 +1,40 @@
+
+
+'use strict';
+
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var moment = require('moment')
+
+  
+  
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
+
+
+
+app.get('/:id',(req,res)=>{
+    
+    let q = Number(req.params.id);
+if(Number.isInteger(q)){
+res.send({
+    "unix":req.params.id,"natural":new Date(q*1000).toDateString()
+})
+}
+else {
+    res.send({"unix":moment(req.params.id).unix(),
+    "natural":new Date(req.params.id).toDateString()})
+}
+
+
+})
+
+
+
+
+
+app.listen(process.env.PORT);
+
